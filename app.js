3e05f0b409de679f9e2ea70c6b8720e15e3e9843
garrettgraber <garrett_graber@yahoo.com>
2014-07-22 18:09:49 -0600

    Just started, not working

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+
+var chunk = function(inArray, chunkSize) {
+
+	var inArrayLength = inArray.length;
+
+	var chunckArrayLength = Math.floor(inArrayLength / chunkSize);
+	var numberChunksNormal = chunckArrayLength * chunkSize;
+	var numberChunksLargerNormal = 0;
+
+
+
+};
