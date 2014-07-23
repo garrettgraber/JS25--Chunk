a4938ae4721b2c191da5a9b3c1c61556d85fc5f2
garrettgraber <garret_graber@yahoo.com>
2014-07-23 01:09:55 -0600

    the function Chunk works fully and so far flawlessly, not yet tested with jasmine

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 
+//Input an array length and the size of the internal arrays
+var arrayCompressor = function(inArrayLength, chunkSize) {
+	
+	var base = Math.floor( inArrayLength / chunkSize );
+	var baseAdd = inArrayLength % chunkSize;
+	var outArray = new Array(chunkSize);
+	
+	//possible map or foreach
+	for(var i=0; i < outArray.length; i++) {
+		outArray[ i ] = base;
+	}
+
+	for(var i=0; i < baseAdd; i++) {
+	
+		var tempValue = outArray[ i ];
+		tempValue += 1;
+		outArray[ i ] = tempValue;
+	
+	}
+	return outArray;
+};
 
 
 var chunk = function(inArray, chunkSize) {
 
-	var inArrayLength = inArray.length;
+	if(inArray.length < chunkSize) {
+		return false;
+	}
 
-	var chunckArrayLength = Math.floor(inArrayLength / chunkSize);
-	var numberChunksNormal = chunckArrayLength * chunkSize;
-	var numberChunksLargerNormal = 0;
-
-
-
+	var internalArrayOfLengths = arrayCompressor(inArray.length, chunkSize);
+	var outArray = [];
+	var startSlice = 0;
+	
+	for(var i=0; i < internalArrayOfLengths.length; i++)	{
+	
+		var endSlice = internalArrayOfLengths[ i ] + startSlice;
+		var tempSlice = inArray.slice( startSlice, endSlice );
+		var startSlice = endSlice;
+		outArray.push( tempSlice );
+	}
+	return outArray;
 };
