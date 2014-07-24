f00d474dd81952ca69e02d69e008cffd718ecdc6
garrettgraber <garrett_graber@yahoo.com>
2014-07-23 19:19:47 -0600

    Rewrote down to 13 lines of good, started at 50 lines with double for loops, now short and sweet and ready for testing.

--- app.js
+++ app.js
@@ -1,45 +1,31 @@
 
 //Input an array length and the size of the internal arrays
 var arrayCompressor = function(inArrayLength, chunkSize) {
-	
 	var base = Math.floor( inArrayLength / chunkSize );
 	var baseAdd = inArrayLength % chunkSize;
 	var outArray = [];
-	
-	//possible map or foreach
 	for(var i=0; i < chunkSize; i++) {
 		outArray.push(base);
 	}
-
-	// outArray = outArray.map(function() {return base;});
-
 	for(var i=0; i < baseAdd; i++) {
-	
 		var tempValue = outArray[ i ];
 		tempValue += 1;
 		outArray[ i ] = tempValue;
-	
 	}
 	return outArray;
 };
 
-
 var chunk = function(inArray, chunkSize) {
-
 	if(inArray.length < chunkSize) {
 		return false;
 	}
-
 	var internalArrayOfLengths = arrayCompressor(inArray.length, chunkSize);
 	var outArray = [];
 	var startSlice = 0;
-	
 	for(var i=0; i < internalArrayOfLengths.length; i++)	{
-	
 		var endSlice = internalArrayOfLengths[ i ] + startSlice;
-		var tempSlice = inArray.slice( startSlice, endSlice );
+		outArray.push( inArray.slice( startSlice, endSlice ) );
 		var startSlice = endSlice;
-		outArray.push( tempSlice );
 	}
 	return outArray;
 };
@@ -52,4 +38,18 @@ var generateArraryIntegers = function(arrayEnd) {
 	}
 	return outArray;
 
+};
+
+var foo = function(inArray, chunkSize) {
+	var base = Math.floor( inArray.length / chunkSize );
+	var baseAdd = inArray.length % chunkSize;
+	var outArray = [];
+	var startSlice = 0;
+	var endSlice;
+	for(var i=0; i < chunkSize; i++) {
+		(i < baseAdd) ? endSlice = startSlice + base + 1 : endSlice = startSlice + base;
+		outArray.push( inArray.slice(startSlice, endSlice) );
+		var startSlice = endSlice;
+	}
+	return outArray;
 };
